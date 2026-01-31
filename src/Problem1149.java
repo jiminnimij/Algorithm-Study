72922d2388c954fa2ca13197e3af43b9d4327ac6
jiminnimij <124450012+jiminnimij@users.noreply.github.com>
2026-02-01 05:58:07 +0900

    [0201] 1149번 - RGB거리

---
+++ src/Problem1149.java
@@ -0,0 +1,41 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Problem1149 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int[][] rgb = new int[n][3];
+        int[][] dp = new int[n][3];
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            rgb[i][0] = Integer.parseInt(st.nextToken());
+            rgb[i][1] = Integer.parseInt(st.nextToken());
+            rgb[i][2] = Integer.parseInt(st.nextToken());
+        }
+
+        dp[0][0] = rgb[0][0];
+        dp[0][1] = rgb[0][1];
+        dp[0][2] = rgb[0][2];
+
+        for (int i = 1; i < n; i++) {
+            dp[i][0] = Math.min(dp[i-1][1], dp[i-1][2]) + rgb[i][0];
+            dp[i][1] = Math.min(dp[i-1][0], dp[i-1][2]) + rgb[i][1];
+            dp[i][2] = Math.min(dp[i-1][0], dp[i-1][1]) + rgb[i][2];
+        }
+
+        int result = Math.min(dp[n-1][0], Math.min(dp[n-1][1], dp[n-1][2]));
+
+        bw.write(String.valueOf(result));
+        bw.flush();
+        bw.close();
+    }
+}
