43cac77be23443215c65eacb7d908c0dbeef8e69
jiminnimij <124450012+jiminnimij@users.noreply.github.com>
2026-03-03 08:22:31 +0900

    [0303] 11055번 - 가장 큰 증가하는 부분 수열

---
+++ src/Problem11055.java
@@ -0,0 +1,40 @@
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Problem11055 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        int max = 0;
+        int[] arr = new int[n];
+        int[] dp = new int[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            dp[i] = 0;
+            for (int j = 0; j < i; j++) {
+                if (arr[i] > arr[j]) {
+                    dp[i] = Math.max(dp[i], dp[j]);
+                }
+            }
+            dp[i] += arr[i];
+
+            if (dp[i] > max) {
+                max = dp[i];
+            }
+        }
+
+        bw.write(String.valueOf(max));
+        bw.flush();
+        bw.close();
+
+    }
+}
