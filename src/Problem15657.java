f54112683e9a6fc0b936af7f8d635d8238c8fc9b
jiminnimij <124450012+jiminnimij@users.noreply.github.com>
2026-04-03 15:29:08 +0900

    [0403] 15657번 - N과 M (8)
    https://www.acmicpc.net/problem/15657

---
+++ src/Problem15657.java
@@ -0,0 +1,47 @@
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Problem15657 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    static int n, m;
+    static int[] arr;
+
+    public static void main(String[] args) throws IOException {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        arr = new int[n];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        backtracking(0, 0, "");
+        bw.flush();
+        bw.close();
+    }
+
+    static void backtracking(int depth, int current, String s) throws IOException {
+        if (depth == m) {
+            bw.write(s);
+            bw.newLine();
+            return;
+        }
+        else {
+            for (int i = current; i < n; i++) {
+                backtracking(depth + 1, i, s + arr[i] + " ");
+            }
+        }
+    }
+}
